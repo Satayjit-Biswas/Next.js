5d139ff4f111d86c67a8347d98a8b6cfcd612369
Satayjit Biswas <61390425+Satayjit-Biswas@users.noreply.github.com>
2022-07-10 02:24:38 +0600

    add about

--- devsstar/components/About/Team.js
+++ devsstar/components/About/Team.js
@@ -1,13 +1,235 @@
+import about_styles from "../../styles/About.module.css";
+import Image from "next/image";
+import img1 from "../../assets/img/about/1.jpg";
+import img2 from "../../assets/img/about/2.jpg";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import { BsTwitter } from "react-icons/bs";
+import { AiFillInstagram } from "react-icons/Ai";
+
 const Team = () => {
     return (
-        <div className="team_area">
+        <div className={`${about_styles.team_area}${" "}top_bottom_gap`}>
             <div className="container">
                 <div className="row">
-                    <div className="col-md-4">
-                        <div className="team_box">
-                            <div className="team_img"></div>
-                            <div className="team_text">
-                                <div className="name">Satayjit Biswas</div>
+                    <div className="col-lg-4 col-md-6 col-sm-6 col-12 mt_15 mb_15">
+                        <div className={about_styles.team_box}>
+                            <div className={about_styles.team_img}>
+                                <a href="#">
+                                    <Image
+                                        src={img1}
+                                        width="400"
+                                        height="400"
+                                    />
+                                </a>
+                            </div>
+                            <div className={about_styles.team_text}>
+                                <div className="name">
+                                    <h2>
+                                        <a href="#">Satayjit Biswas</a>
+                                    </h2>
+                                </div>
+                                <div className="position">
+                                    <h4>Co-founder</h4>
+                                </div>
+                                <div className={about_styles.social}>
+                                    <a href="#">
+                                        <FaFacebookF />
+                                    </a>
+                                    <a href="#">
+                                        <AiFillInstagram />
+                                    </a>
+                                    <a href="#">
+                                        <BsTwitter />
+                                    </a>
+                                    <a href="#">
+                                        <FaLinkedinIn />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-md-6 col-sm-6 col-12 mt_15 mb_15">
+                        <div className={about_styles.team_box}>
+                            <div className={about_styles.team_img}>
+                                <a href="#">
+                                    <Image
+                                        src={img2}
+                                        width="400"
+                                        height="400"
+                                    />
+                                </a>
+                            </div>
+                            <div className={about_styles.team_text}>
+                                <div className="name">
+                                    <h2>
+                                        <a href="#">Satayjit Biswas</a>
+                                    </h2>
+                                </div>
+                                <div className="position">
+                                    <h4>Co-founder</h4>
+                                </div>
+                                <div className={about_styles.social}>
+                                    <a href="#">
+                                        <FaFacebookF />
+                                    </a>
+                                    <a href="#">
+                                        <AiFillInstagram />
+                                    </a>
+                                    <a href="#">
+                                        <BsTwitter />
+                                    </a>
+                                    <a href="#">
+                                        <FaLinkedinIn />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-md-6 col-sm-6 col-12 mt_15 mb_15">
+                        <div className={about_styles.team_box}>
+                            <div className={about_styles.team_img}>
+                                <a href="#">
+                                    <Image
+                                        src={img2}
+                                        width="400"
+                                        height="400"
+                                    />
+                                </a>
+                            </div>
+                            <div className={about_styles.team_text}>
+                                <div className="name">
+                                    <h2>
+                                        <a href="#">Satayjit Biswas</a>
+                                    </h2>
+                                </div>
+                                <div className="position">
+                                    <h4>Co-founder</h4>
+                                </div>
+                                <div className={about_styles.social}>
+                                    <a href="#">
+                                        <FaFacebookF />
+                                    </a>
+                                    <a href="#">
+                                        <AiFillInstagram />
+                                    </a>
+                                    <a href="#">
+                                        <BsTwitter />
+                                    </a>
+                                    <a href="#">
+                                        <FaLinkedinIn />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-md-6 col-sm-6 col-12 mt_15 mb_15">
+                        <div className={about_styles.team_box}>
+                            <div className={about_styles.team_img}>
+                                <a href="#">
+                                    <Image
+                                        src={img2}
+                                        width="400"
+                                        height="400"
+                                    />
+                                </a>
+                            </div>
+                            <div className={about_styles.team_text}>
+                                <div className="name">
+                                    <h2>
+                                        <a href="#">Satayjit Biswas</a>
+                                    </h2>
+                                </div>
+                                <div className="position">
+                                    <h4>Co-founder</h4>
+                                </div>
+                                <div className={about_styles.social}>
+                                    <a href="#">
+                                        <FaFacebookF />
+                                    </a>
+                                    <a href="#">
+                                        <AiFillInstagram />
+                                    </a>
+                                    <a href="#">
+                                        <BsTwitter />
+                                    </a>
+                                    <a href="#">
+                                        <FaLinkedinIn />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-md-6 col-sm-6 col-12 mt_15 mb_15">
+                        <div className={about_styles.team_box}>
+                            <div className={about_styles.team_img}>
+                                <a href="#">
+                                    <Image
+                                        src={img2}
+                                        width="400"
+                                        height="400"
+                                    />
+                                </a>
+                            </div>
+                            <div className={about_styles.team_text}>
+                                <div className="name">
+                                    <h2>
+                                        <a href="#">Satayjit Biswas</a>
+                                    </h2>
+                                </div>
+                                <div className="position">
+                                    <h4>Co-founder</h4>
+                                </div>
+                                <div className={about_styles.social}>
+                                    <a href="#">
+                                        <FaFacebookF />
+                                    </a>
+                                    <a href="#">
+                                        <AiFillInstagram />
+                                    </a>
+                                    <a href="#">
+                                        <BsTwitter />
+                                    </a>
+                                    <a href="#">
+                                        <FaLinkedinIn />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-md-6 col-sm-6 col-12 mt_15 mb_15">
+                        <div className={about_styles.team_box}>
+                            <div className={about_styles.team_img}>
+                                <a href="#">
+                                    <Image
+                                        src={img2}
+                                        width="400"
+                                        height="400"
+                                    />
+                                </a>
+                            </div>
+                            <div className={about_styles.team_text}>
+                                <div className="name">
+                                    <h2>
+                                        <a href="#">Satayjit Biswas</a>
+                                    </h2>
+                                </div>
+                                <div className="position">
+                                    <h4>Co-founder</h4>
+                                </div>
+                                <div className={about_styles.social}>
+                                    <a href="#">
+                                        <FaFacebookF />
+                                    </a>
+                                    <a href="#">
+                                        <AiFillInstagram />
+                                    </a>
+                                    <a href="#">
+                                        <BsTwitter />
+                                    </a>
+                                    <a href="#">
+                                        <FaLinkedinIn />
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
