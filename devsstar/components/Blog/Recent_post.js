3cf47a5fc5a7b4bd88deefb848d596b4a27a4eb4
Satayjit Biswas <61390425+Satayjit-Biswas@users.noreply.github.com>
2022-07-19 01:55:24 +0600

    recent post

---
+++ devsstar/components/Blog/Recent_post.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Recent_post = () => {
+    return (
+        <div className={recentpost_date}>
+            <div className="recentpost_date">{blog_date}</div>
+        </div>
+    );
+};
+
+export default Recent_post;
