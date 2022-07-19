b4af2a60aaac1268839ef85ec9c3d9f3d02a0408
Satayjit Biswas <61390425+Satayjit-Biswas@users.noreply.github.com>
2022-07-20 02:11:21 +0600

    add login

--- devsstar/components/Blog/Post.js
+++ devsstar/components/Blog/Post.js
@@ -6,17 +6,121 @@ import { GrFacebookOption } from "react-icons/gr";
 
 const Post = () => {
     return (
-        <div className={`${blog_styles.blog_area}${" "}top_bottom_gap`}>
+        <div>
             <div className={blog_styles.post_area_box}>
                 <div className="row">
-                    <div className="col-lg-5">
+                    <div className="col-lg-5 mt_15 mb_15">
                         <div className={blog_styles.post_img}>
                             <a href="#">
                                 <Image src={p1} width="360" height="260" />
                             </a>
                         </div>
                     </div>
-                    <div className="col-lg-7">
+                    <div className="col-lg-7 mt_15 mb_15">
+                        <h2 className={blog_styles.post_title}>
+                            <a href="#">
+                                Xterra ERG220 Rower: The Best Budget Priced
+                                Rowing Machine
+                            </a>
+                        </h2>
+                        <div className={blog_styles.post_date}>
+                            <span className="mr_20">
+                                Categories : Reactcsdf
+                            </span>
+                            <span>21/20/22</span>
+                        </div>
+                        <div className={blog_styles.post_details}>
+                            {/* <p>{blog_conclusion.substring(0, 160)} ....</p> */}
+                            <p>
+                                If you’ve been searching for a cheap rowing
+                                machine and the Xterra ERG220 Rower sounds like…
+                            </p>
+                        </div>
+                        <div className={blog_styles.post_read_more}>
+                            <div className="custom_btn">
+                                <a href="#">read more</a>
+                            </div>
+                        </div>
+                        <div className={blog_styles.post_icon}>
+                            <div className={blog_styles.post_icon_box}>
+                                <a href="#" target="_blank">
+                                    <BsYoutube />
+                                </a>
+                                <a href="#" target="_blank">
+                                    <GrFacebookOption />
+                                </a>
+                                <a href="#" target="_blank">
+                                    <BsTwitter />
+                                </a>
+                                <a href="#" target="_blank">
+                                    <BsInstagram />
+                                </a>
+                                <a href="#" target="_blank">
+                                    <BsGithub />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-5 mt_15 mb_15">
+                        <div className={blog_styles.post_img}>
+                            <a href="#">
+                                <Image src={p1} width="360" height="260" />
+                            </a>
+                        </div>
+                    </div>
+                    <div className="col-lg-7 mt_15 mb_15">
+                        <h2 className={blog_styles.post_title}>
+                            <a href="#">
+                                Xterra ERG220 Rower: The Best Budget Priced
+                                Rowing Machine
+                            </a>
+                        </h2>
+                        <div className={blog_styles.post_date}>
+                            <span className="mr_20">
+                                Categories : Reactcsdf
+                            </span>
+                            <span>21/20/22</span>
+                        </div>
+                        <div className={blog_styles.post_details}>
+                            {/* <p>{blog_conclusion.substring(0, 160)} ....</p> */}
+                            <p>
+                                If you’ve been searching for a cheap rowing
+                                machine and the Xterra ERG220 Rower sounds like…
+                            </p>
+                        </div>
+                        <div className={blog_styles.post_read_more}>
+                            <div className="custom_btn">
+                                <a href="#">read more</a>
+                            </div>
+                        </div>
+                        <div className={blog_styles.post_icon}>
+                            <div className={blog_styles.post_icon_box}>
+                                <a href="#" target="_blank">
+                                    <BsYoutube />
+                                </a>
+                                <a href="#" target="_blank">
+                                    <GrFacebookOption />
+                                </a>
+                                <a href="#" target="_blank">
+                                    <BsTwitter />
+                                </a>
+                                <a href="#" target="_blank">
+                                    <BsInstagram />
+                                </a>
+                                <a href="#" target="_blank">
+                                    <BsGithub />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-5 mt_15 mb_15">
+                        <div className={blog_styles.post_img}>
+                            <a href="#">
+                                <Image src={p1} width="360" height="260" />
+                            </a>
+                        </div>
+                    </div>
+                    <div className="col-lg-7 mt_15 mb_15">
                         <h2 className={blog_styles.post_title}>
                             <a href="#">
                                 Xterra ERG220 Rower: The Best Budget Priced
--- devsstar/pages/blog.js
+++ devsstar/pages/blog.js
@@ -1,21 +1,33 @@
 import React from "react";
 import Post from "../components/Blog/Post";
 import Page_header from "../components/Public/Page_header";
+import blog_styles from "../styles/Blog.module.css";
+import google_ads from "../styles/google_ads.module.css";
 
 const blog = () => {
     return (
         <div>
             <Page_header text="Our blog"></Page_header>
-            <div className="blog_area">
+            <div className={`${blog_styles.blog_area}${" "}top_bottom_gap`}>
                 <div className="container">
                     <div className="blog_area_box">
                         <div className="row">
                             <div className="col-lg-8">
-                                <div className="blog_left_site">
+                                <div
+                                    className={`${
+                                        blog_styles.blog_left_site
+                                    }${" "}${google_ads.blog_left_site}`}
+                                >
                                     <div className="d-flex justify-content-center">
-                                        <div className="ads728_90"></div>
-                                        <div className="ads468_60"></div>
-                                        <div className="ads320_100"></div>
+                                        <div
+                                            className={google_ads.ads728_90}
+                                        ></div>
+                                        <div
+                                            className={google_ads.ads468_60}
+                                        ></div>
+                                        <div
+                                            className={google_ads.ads320_100}
+                                        ></div>
                                     </div>
                                     <Post></Post>
                                     {/* {loading ? (
@@ -36,26 +48,51 @@ const blog = () => {
                                     ></Post>
                                 )} */}
                                     <div className="d-flex justify-content-center">
-                                        <div className="ads728_90"></div>
-                                        <div className="ads468_60"></div>
-                                        <div className="ads320_100"></div>
+                                        <div
+                                            className={google_ads.ads728_90}
+                                        ></div>
+                                        <div
+                                            className={google_ads.ads468_60}
+                                        ></div>
+                                        <div
+                                            className={google_ads.ads320_100}
+                                        ></div>
                                     </div>
                                 </div>
                             </div>
                             <div className="col-lg-4">
-                                <div className="blog_right_site ml_20 mb_10">
+                                <div
+                                    className={`${
+                                        blog_styles.blog_right_site
+                                    }${" "}${google_ads.blog_right_site}`}
+                                >
                                     <div className="d-flex justify-content-center">
-                                        <div className="ads320_50"></div>
-                                        <div className="ads468_60"></div>
-                                        <div className="ads250_250"></div>
-                                        <div className="ads320_100"></div>
+                                        <div
+                                            className={google_ads.ads320_50}
+                                        ></div>
+                                        <div
+                                            className={google_ads.ads468_60}
+                                        ></div>
+                                        <div
+                                            className={google_ads.ads250_250}
+                                        ></div>
+                                        <div
+                                            className={google_ads.ads320_100}
+                                        ></div>
                                     </div>
+                                    <h3 className="upper_line">RECENT POSTS</h3>
                                     {/* <RecentPost></RecentPost> */}
                                     <div className="d-flex justify-content-center">
-                                        <div className="ads468_60"></div>
-                                        <div className="ads320_100"></div>
+                                        <div
+                                            className={google_ads.ads468_60}
+                                        ></div>
+                                        <div
+                                            className={google_ads.ads320_100}
+                                        ></div>
 
-                                        <div className="ads336_280"></div>
+                                        <div
+                                            className={google_ads.ads336_280}
+                                        ></div>
                                     </div>
                                     {/* <Categories
                                     filterBlog={(e) => filterblog(e)}
