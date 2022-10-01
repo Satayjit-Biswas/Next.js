dda88f52f95545862a1fd335de1ae91ec0fa52e8
Satayjit Biswas <61390425+Satayjit-Biswas@users.noreply.github.com>
2022-10-01 15:16:38 +0600

    all responsive

--- devsstar/components/Account/Forgot_pass.js
+++ devsstar/components/Account/Forgot_pass.js
@@ -1,13 +1,21 @@
 import Login_styles from "../../styles/Login.module.css";
+import { useRef } from "react";
 
 const Forgot_pass = () => {
+    const click_account = useRef();
+    const drop_form = (e) => {
+        click_account.current.classList.toggle(`${Login_styles.form_showed}`);
+    };
     return (
         <div className={`${Login_styles.login_form_area}${" "}top_bottom_gap`}>
             <div className="container">
                 <div className="mt_15 mb_15">
                     <div className={Login_styles.login_form_area_box}>
-                        <div className={Login_styles.form_left}>
-                            <h2>Remember Password</h2>
+                        <div
+                            className={`${Login_styles.form_left} ${Login_styles.Back}`}
+                            ref={click_account}
+                        >
+                            <h2 onClick={drop_form}>Remember Password</h2>
                             <button type="submit">Go Back</button>
                         </div>
                         <div className={Login_styles.form_right}>
--- devsstar/components/Account/Login_reg.js
+++ devsstar/components/Account/Login_reg.js
@@ -1,14 +1,21 @@
+import { useRef } from "react";
 import Login_styles from "../../styles/Login.module.css";
-
 const Login_reg = () => {
+    const click_account = useRef();
+    const drop_form = (e) => {
+        click_account.current.classList.toggle(`${Login_styles.form_showed}`);
+    };
     return (
         <div className={`${Login_styles.login_form_area}${" "}top_bottom_gap`}>
             <div className="container">
                 <div className="mt_15 mb_15">
                     <div className={Login_styles.login_form_area_box}>
-                        <div className={Login_styles.form_left}>
-                            <h2>Creating Account</h2>
-                            <form action="">
+                        <div
+                            className={`${Login_styles.form_left} `}
+                            ref={click_account}
+                        >
+                            <h2 onClick={drop_form}>Creating Account</h2>
+                            <form action="" className="mt_20">
                                 <div className={Login_styles.input_box}>
                                     <label>Your Name</label>
                                     <input
